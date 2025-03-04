27f0623f35a612bbbe27b43bc3e8d3cc18768c23
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-03-03 18:08:09 -0600

    add searching files track by git, and disabled vimsneak

--- init.lua
+++ init.lua
@@ -427,6 +427,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing [B]uffers' })
+      vim.keymap.set('n', '<leader>g', builtin.git_files, { desc = 'Search in [G]it Files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
--- lua/custom/plugins/vim-sneak.lua
+++ lua/custom/plugins/vim-sneak.lua
@@ -2,6 +2,8 @@
 -- https://github.com/justinmk/vim-sneak
 return {
   'justinmk/vim-sneak',
+  enabled = false,
+  lazy = false,
   init = function()
     vim.g['sneak#label'] = 1
   end,
